a3c876f052023206c75f787b70caba36431be3aa
Yu <chia3120@hotmail.com>
2022-09-01 15:55:48 +0800

    1. Finished delete task. 2. Finished edit task.

--- src/app/todolist/todolist.component.ts
+++ src/app/todolist/todolist.component.ts
@@ -14,13 +14,36 @@ export class TodolistComponent implements OnInit {
     {complete: false, description:'YYY'},
     {complete: true, description:'ZZZ'}
   ]
-
+  editingIndex: number | null = null;
+  editingDescription: string = '';
   constructor(
     private fb: FormBuilder,
   ) { }
 
 
   ngOnInit(): void {
+  }
+
+  addTask() {
+    const newtask = {
+      complete: false, description:this.value
+    };
+    this.taskList.push(newtask)
+  }
+
+  editTask(i: number) {
+    this.editingIndex = i;
+    this.editingDescription = this.taskList[i]['description'];
+  }
+
+  deleteTask(i: number) {
+    this.taskList.splice(i, 1);
+  }
+
+  saveTask(i: number) {
+    this.taskList[i]['description'] = this.editingDescription;
+    this.editingIndex = null;
+    console.log(this.editingIndex);
   }
 
 }
